48236578768e7d384122f164e462288436384551
John Praesto <john_praesto@hotmail.com>
2023-11-03 16:25:46 +0100

    Add project files.

---
+++ Geometry.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3_Polymorphism
+{
+    internal class Geometry
+    {
+        public virtual double Area() // Måste returnera något. Men jag vill inte att den ska returnera något.
+        {                           // Men den måste ändå returna double eftersom arv-klasserna vill returna double
+            return 1;         
+        }
+    }
+    class Rectangle : Geometry
+    {
+        private double _side1; // Sätter till private för jag vill inte att användaren ska inputta nåt tokigt i Main
+        private double _side2;
+        public Rectangle(double side1, double side2)
+        {
+            Side1 = side1;
+            Side2 = side2;
+        }
+        public double Side1 // Det här är en "Property"
+        {
+            get { return _side1; } // Vad gör denna rad?
+            set
+            { 
+                if(value < 0) // Om användaren sätter en sida till mindre än 0 så ändras värdet till 0
+                {
+                    _side1 = 0;
+                }
+                else
+                {
+                    _side1 = value;
+                }
+            }
+        }
+        public double Side2
+        {
+            get { return _side2; }
+            set
+            {
+                if (value < 0)
+                {
+                    _side2 = 0;
+                }
+                else
+                {
+                    _side2 = value;
+                }
+            }
+        }
+        public override double Area()
+        {
+            return _side1 * _side2;
+        }
+    }
+    class Square : Geometry
+    {
+        private double _side;
+        public Square(double side)
+        {
+            Side = side;
+        }
+        public double Side
+        {
+            get { return _side; }
+            set
+            {
+                if (value < 0)
+                {
+                    _side = 0;
+                }
+                else
+                {
+                    _side = value;
+                }
+            }
+        }
+        public override double Area()
+        {
+            return _side * _side;
+        }
+    }
+    class Circle : Geometry
+    {
+        private double _radius;
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius
+        {
+            get { return _radius; }
+            set
+            {
+                if (value < 0)
+                {
+                    _radius = 0;
+                }
+                else
+                {
+                    _radius = value;
+                }
+            }
+        }
+        public override double Area()
+        {
+            return _radius * _radius * Math.PI;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Labb_3_Polymorphism
+{
+    ///////////////////////////////////////////////////////////
+    /////////////////  JOHN PRAESTO SUT-23  ///////////////////
+    ///////////////////////////////////////////////////////////
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Ange sida ett på rektangel: ");
+                double side1 = Converter();
+
+                Console.Write("Ange sida två på rektangel: ");
+                double side2 = Converter();
+                Geometry rectangle = new Rectangle(side1, side2); // Vad spelar det för roll vilken klass jag inleder med?
+
+                Console.Write("Ange sida på kvadrat: ");
+                side1 = Converter();
+                Square square = new Square(side1); // Vad blir skillnaden lixom?
+
+                Console.Write("Ange diameter på cirkel: ");
+                side1 = Converter();
+                side1 = side1 / 2;
+                Geometry circle = new Circle(side1);
+
+                Console.WriteLine("\nArea of rectangle: " + rectangle.Area());
+                Console.WriteLine("Area of square: " + square.Area());
+                Console.WriteLine("Area of circle: " + circle.Area());
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        public static double Converter()
+        {
+            double side = 0;
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                try
+                {
+                    side = double.Parse(userInput);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Du har matat in ogiltiga tecken. Mata endast in heltal eller decimaltal.");
+                }
+            }
+            return side;
+        }
+    }
+}
